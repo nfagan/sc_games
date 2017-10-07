c329ebb4ca77f31a921d3c6af1573b7401358493
Emily Cohodes <emily.cohodes@yale.edu>
2017-10-07 15:49:29 -0400

    Updated Labjack test script to work with all FIO ports on Labjack

--- resources/TestLabjack.py
+++ resources/TestLabjack.py
@@ -3,10 +3,15 @@ from datetime import datetime
 from time import sleep
 import u3
 
+try:
+	fio_num = int(sys.argv[1])
+	assert fio_num in (4, 5, 6, 7)
+except:
+	print "This script tests Labjack FIO ports 4-7"
+	print "Usage: python %s 4 (or 5-7)" % __file__
+	sys.exit()
 
 # make sure Exodriver (from Labjack website) and the U3 package are installed before using
-fio_num = 5
-
 try:
 	d = u3.U3()
 except:
@@ -16,11 +21,11 @@ except:
 d.getCalibrationData()
 timestamp = str(datetime.now())
 d.setFIOState(fio_num, state=1)
-print "Set FIOS%d to \"high\" at %s." % (fio_num, timestamp)
+print "Set FIO%d to \"high\" at %s." % (fio_num, timestamp)
 sleep(1)
 print "Waiting..."
 sleep(2)
 timestamp = str(datetime.now())
 d.setFIOState(fio_num, state=0)
-print "Set FIOS%d to \"low\" (off) at %s." % (fio_num, timestamp)
+print "Set FIO%d to \"low\" (off) at %s." % (fio_num, timestamp)
 
