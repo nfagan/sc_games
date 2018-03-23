0e1d8a4cf5ef1f16b6781f6da3be70f8091364dc
Jeff Mandell <Jeff@mandells.org>
2018-03-22 23:05:54 -0400

    Separate timing files now get created for first and second scans, with mri trigger files parsed to find first/second mri start times separately

--- scripts/make_mundane_timing_files.py
+++ scripts/make_mundane_timing_files.py
@@ -14,11 +14,11 @@ output_dir = re.sub('\/+$', '', output_dir) # strip trailing slashes, if any
 mri = args.mri # file handle for mri trigger file
 subject = args.subject_id
 
-# define output files
-aversive_timing_file = output_dir + '/aversive_noise_timing_%s.txt' % subject
-antic_timing_file = output_dir + '/antic_period_timing_%s.txt' % subject
-avoid_timing_file = output_dir + '/avoid_period_timing_%s.txt' % subject
-intersection_timing_file = output_dir + '/intersection_timing_%s.txt' % subject
+# define output file prefixes (more gets added later)
+aversive_timing_file = output_dir + '/aversive_noise_timing'
+antic_timing_file = output_dir + '/antic_period_timing'
+avoid_timing_file = output_dir + '/avoid_period_timing'
+intersection_timing_file = output_dir + '/intersection_timing'
 
 # make sure subject ID makes sense
 if len(subject) != 3:
@@ -35,12 +35,33 @@ if os.path.isdir(output_dir):
 	print("Error: Output directory already exists.")
 	sys.exit()
 
-mri_start_time = float(mri.readline().strip().split("\t")[-1])
 
+## Find MRI start times for first and second scans (basing second start time on long break)
 #adjusting for subtraction of 8 volumes from beginning of scan due to conversation with PO 3/20/18 (8 TRs x .8 seconds per TR)
+first_mri_start_time = float(mri.readline().strip().split("\t")[-1])
+previous_time = first_mri_start_time
+first_mri_start_time += 6.4
+second_mri_start_time = None
 
-mri_start_time = mri_start_time + 6.4
+for line in mri:
+	fields = line.strip().split("\t")
+	trigger_num = int(fields[0])
+	timepoint = float(fields[1])
+	# a long break in TRs indicates start of new scan
+	if timepoint - previous_time > 60:
+		second_mri_start_time = timepoint
+		if trigger_num != 374: # expected start of second scan 
+			print("Warning: Second scan was expected to start with trigger number 374, but it seems to have started at trigger %d." % trigger_num)
+		break
+	previous_time = timepoint
+if not second_mri_start_time:
+	print("Error: Could not deduce start time of the second MRI scan from the trigger file.")
+	sys.exit()
+second_mri_start_time += 6.4
+mri_start_times = (first_mri_start_time, second_mri_start_time)
 
+
+# make output directory
 try:
 	os.mkdir(output_dir)
 except OSError:
@@ -48,7 +69,7 @@ except OSError:
 	sys.exit()
 
 
-# skip to header line and find relevant columns
+# skip to header line of data file and find relevant columns
 for line in f:
 	if re.search('^#', line):
 		continue
@@ -58,29 +79,32 @@ for line in f:
 	avoid_period_onset_col = header_fields.index('avoid_period_onset')
 	intersection_col = header_fields.index('touching_target_object')
 	time_col = header_fields.index('time_of_day_in_seconds')
+	trial_col = header_fields.index('trial_num')
 	break
 
-# track events
-aversive_noise_events = []
-antic_period_events = []
-avoid_period_events = []
-intersection_events = []
+# track events (one list for first half of trials, one list for second half of trials)
+aversive_noise_events = [[], []]
+antic_period_events = [[], []]
+avoid_period_events = [[], []]
+intersection_events = [[], []]
 
-def convert_time(t):
-	return str(float(t) - mri_start_time)
+def convert_time(t, part):
+	return str(float(t) - mri_start_times[part])
 
 # process data
 current_intersection_start_time = None
 
 for line in f:
 	fields = line.split("\t")
-	time_in_task = convert_time(fields[time_col])
+	trial_num = int(fields[trial_col])
+	part = 0 if trial_num < 16 else 1 # 0 = first half, 1 = second half
+	time_in_task = convert_time(fields[time_col], part)
 	if fields[aversive_noise_onset_col] == '1':
-		aversive_noise_events.append((time_in_task, '.5')) # duration = .5 for noise
+		aversive_noise_events[part].append((time_in_task, '.5')) # duration = .5 for noise
 	if fields[antic_period_onset_col] == '1':
-		antic_period_events.append((time_in_task, '4.0')) # duration = 4.0 for anticipatory period
+		antic_period_events[part].append((time_in_task, '4.0')) # duration = 4.0 for anticipatory period
 	if fields[avoid_period_onset_col] == '1':
-		avoid_period_events.append((time_in_task, '4.5')) # duration = 4.5 for avoidance period
+		avoid_period_events[part].append((time_in_task, '4.5')) # duration = 4.5 for avoidance period
 	if fields[intersection_col] == '1':
 		if current_intersection_start_time:
 			continue
@@ -88,14 +112,18 @@ for line in f:
 			current_intersection_start_time = float(time_in_task)
 	elif current_intersection_start_time:
 		intersection_duration = str(float(time_in_task) - current_intersection_start_time) # approximately, at least
-		intersection_events.append((current_intersection_start_time, intersection_duration))
+		intersection_events[part].append((current_intersection_start_time, intersection_duration))
 		current_intersection_start_time = None
+	f_pos = f.tell()
+
 
 for events, file in ((aversive_noise_events, aversive_timing_file), (antic_period_events, antic_timing_file),
   (avoid_period_events, avoid_timing_file), (intersection_events, intersection_timing_file)):
-	with open(file, 'w') as w:
-		for time, duration in events:
-			w.write("%s\t%s\t1\n" %(time, duration)) # FSL wants a 1 in every row
+	for part in (0, 1):
+		output_file = file + '_sc2p' + str(part + 1) + "_%s.txt" % subject
+		with open(output_file, 'w') as w:
+			for time, duration in events[part]:
+				w.write("%s\t%s\t1\n" %(time, duration)) # FSL wants a 1 in every row
 	
 
 
